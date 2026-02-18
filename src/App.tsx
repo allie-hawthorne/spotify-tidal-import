4696b064f287bac4e9b5a5e2f5848ff06852c5a0
Allie <me@alliehawthorne.dev>
2026-02-18 16:05:50 +0000

    dealing with the nightmare that was tidal auth

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
-import { useEffect, useState } from 'react';
-import { spotifyApi } from './ids'
+import { useCallback, useEffect, useState } from 'react';
+import { initialiseTidal, spotifyApi, tidalApi } from './ids'
 import type { AccessToken } from '@spotify/web-api-ts-sdk';
 import { SpotifyLoginButton } from './components/SpotifyLoginButton';
 import { TidalLoginButton } from './components/TidalLoginButton';
@@ -12,6 +12,16 @@ function App() {
     spotifyApi.getAccessToken().then(setSpotifyToken).catch(console.error)
   }, []);
 
+  useEffect(() => {
+    initialiseTidal().then(() => console.log('Tidal authentication successful')).catch(console.error);
+  }, []);
+
+  const handleTidalClick = useCallback(() => {
+    tidalApi.GET('/users/me').then(data => {
+      console.log('Tidal authentication successful', data);
+    }).catch(console.error)
+  }, []);
+
   return <div className='flex flex-col gap-2'>
     <h1>Spotify Tidal Importer</h1>
     {spotifyToken
@@ -22,6 +32,7 @@ function App() {
       ? <p>Logged in to Tidal!</p>
       : <TidalLoginButton />
     }
+    <button onClick={handleTidalClick}>Click Mee</button>
   </div>
 }
 
--- src/ids.ts
+++ src/ids.ts
@@ -9,13 +9,20 @@ export const spotifyApi = SpotifyApi.withUserAuthorization(SPOTIFY_CLIENT_ID, SP
 const TIDAL_API_KEY = 'TDiZl2ekSxNNwrYL';
 const TIDAL_REDIRECT_URI = 'http://127.0.0.1:5500/tidal/';
 
+const tidalParams = {
+    clientId: TIDAL_API_KEY,
+    credentialsStorageKey: 'authorisationCode',
+    scopes: ['user.read']   
+};
+
 export const tidalApi = createAPIClient(credentialsProvider);
+export const initialiseTidal = () => init(tidalParams);
 export const authenticateTidal = async () => {
-    await init({
-        clientId: TIDAL_API_KEY,
-        credentialsStorageKey: 'authorisationCode',
-        scopes: ['user.read']
-    }).then(() => console.log('Tidal authentication successful'));
+    await init(tidalParams)
+
+    // We need these on the static redirect page
+    localStorage.setItem('tidalClientId', TIDAL_API_KEY);
+    localStorage.setItem('tidalRedirectUri', TIDAL_REDIRECT_URI);
 
     const loginUrl = await initializeLogin({redirectUri: TIDAL_REDIRECT_URI});
 
