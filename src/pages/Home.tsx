4b335dddcebb4c917a87a91826f0e9e7f41c225e
Allie <me@alliehawthorne.dev>
2026-02-18 16:58:49 +0000

    minor code cleanup

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,5 +1,5 @@
 export const Main = () => {
-    return <div className='flex flex-col gap-2'>
-      <p>Logged in to both Spotify and Tidal!</p>
-    </div>
+  return <div className='flex flex-col gap-2'>
+    <p>Logged in to both Spotify and Tidal!</p>
+  </div>;
 }
