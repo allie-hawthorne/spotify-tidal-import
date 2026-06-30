c69d522f998420d801533df7465be50de172c4f7
Allie <me@alliehawthorne.dev>
2026-06-30 02:08:02 +0100

    adding selection of items to import

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,29 +1,26 @@
+import { useState, type HTMLAttributes, type PropsWithChildren } from "react";
 import { AllArtists } from "./EasyImport/AllArtists"
 import { AllPlaylists } from "./EasyImport/AllPlaylists"
 
-// Order Matters!
-// const PageMap = [
-//   {name: 'Playlists', component: <Playlists />},
-//   {name: 'Albums', component: null},
-// ] as const
-
 export const Home = () => {
-  return <div className="flex flex-col gap-2">
-    <div className="flex gap-2 justify-center">
-      {/* TODO: Add import from dropdown etc */}
-      <div>
-        <AllPlaylists />
-        <AllArtists />
-      </div>
-      {/* {PageMap.map(({name}, i) => <>
-        <button className={`${i === pageIndex && 'text-violet-400'} cursor-pointer`}
-          key={i}
-          onClick={() => setPageIndex(i)}
-        >
-          {name}
-        </button>
-      </>)} */}
-    </div>
-    {/* {PageMap[pageIndex].component} */}
+  const [importPlaylists, setImportPlaylists] = useState(true);
+  const [importArtists, setImportArtists] = useState(true);
+
+  return <div className="flex gap-2 flex-col">
+    {/* TODO: Add import from dropdown etc */}
+    <ItemWrapper onClick={() => setImportPlaylists(!importPlaylists)}>
+      <input type="checkbox" checked={importPlaylists} />
+      <AllPlaylists />
+    </ItemWrapper>
+    <ItemWrapper onClick={() => setImportArtists(!importArtists)}>
+      <input type="checkbox" checked={importArtists} />
+      <AllArtists />
+    </ItemWrapper>
+  </div>
+}
+
+const ItemWrapper = ({children, ...props}: PropsWithChildren<HTMLAttributes<HTMLDivElement>>) => {
+  return <div className="flex gap-2 touch-none cursor-pointer" {...props}>
+    {children}
   </div>
 }
