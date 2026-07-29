b67b35341fad7c53d0043057d2cb2f96da139492
Allie <me@alliehawthorne.dev>
2026-07-29 12:36:04 +0100

    converting to csv before zipping

--- src/components/ExportButton.tsx
+++ src/components/ExportButton.tsx
@@ -1,16 +1,17 @@
 import { Button } from "./Button";
 import { useSpotify } from "../api-helpers/SpotifyContext";
 import JSZip from "jszip";
+import Papa from "papaparse";
 
 export const ExportButton = () => {
   const {isLoading, albumData, artistData, playlistData, trackData} = useSpotify();
 
   const onExportClick = async () => {
     const zip = new JSZip();
-    zip.file('saved-albums.json', JSON.stringify(albumData.items));
-    zip.file('saved-artists.json', JSON.stringify(artistData.items));
-    zip.file('playlists.json', JSON.stringify(playlistData.items));
-    zip.file('saved-tracks.json', JSON.stringify(trackData.items));
+    zip.file('saved-albums.csv', Papa.unparse(albumData.items));
+    zip.file('saved-artists.csv', Papa.unparse(artistData.items));
+    zip.file('playlists.csv', Papa.unparse(playlistData.items));
+    zip.file('saved-tracks.csv', Papa.unparse(trackData.items));
 
     const blob = await zip.generateAsync({type: 'blob'});
     const encodedUri = window.URL.createObjectURL(blob);
