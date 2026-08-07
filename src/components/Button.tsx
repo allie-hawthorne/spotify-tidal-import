15a88340627f31a90759678f6ffe3c316e84e811
Allie <me@alliehawthorne.dev>
2026-08-07 22:03:02 +0100

    slightly purple button

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,7 +1,7 @@
 import type { ButtonHTMLAttributes } from "react";
 
 export const Button = ({ children, ...props }: ButtonHTMLAttributes<HTMLButtonElement>) => {
-  return <button className="bg-gray-100 not-disabled:hover:bg-gray-300 disabled:opacity-50 disabled:cursor-not-allowed duration-300 text-black text-sm rounded-xl px-3 py-1 cursor-pointer" {...props}>
+  return <button className="bg-purple-200 not-disabled:hover:bg-purple-300 disabled:opacity-50 disabled:cursor-not-allowed duration-300 text-black text-sm rounded-xl px-3 py-1 cursor-pointer" {...props}>
     {children}
   </button>;
 }
