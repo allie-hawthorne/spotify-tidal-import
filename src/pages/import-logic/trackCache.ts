e783317bce75dc7f385c2e016b11547c835ee605
Allie <me@alliehawthorne.dev>
2026-08-14 11:52:05 +0100

    code cleanup

--- src/pages/import-logic/trackCache.ts
+++ src/pages/import-logic/trackCache.ts
@@ -1,38 +1,27 @@
 import type { ITrack } from "../../types";
 import type { ImportTracksParams } from "./findTracks";
 
-type CacheMap = [string, ITrack | string][]
-
 const CACHE_KEY_PREFIX = 'spotify-track-cache';
 const CACHE_MATCH_FAIL_MARKER = 'no-match';
 
 const getKey = (id: string) => `${CACHE_KEY_PREFIX}:${id}`;
 
-const cache = (cacheMap: CacheMap) => {
-  cacheMap.forEach(([sId, tTrack]) => {
-    const cacheString = typeof tTrack === "string" ? tTrack : JSON.stringify(tTrack);
-    localStorage.setItem(getKey(sId), cacheString);
-  });
-};
-export const cacheFail = (id: string) => cache([[id, CACHE_MATCH_FAIL_MARKER]]);
-export const cacheTrack = (id: string, track: ITrack) => cache([[id, track]]);
+const cache = (id: string, val: string) => localStorage.setItem(getKey(id), val);
+export const cacheTrack = (id: string, track: ITrack) => cache(id, JSON.stringify(track));
+export const cacheFail = (id: string) => cache(id, CACHE_MATCH_FAIL_MARKER);
 
 export const cacheFromIsrc = (tidalTracks: ITrack[], spotifyTracks: ITrack[]) => {
-  const cacheMap: CacheMap = [];
   tidalTracks.forEach(tt => {
     const spotifyTrack = spotifyTracks.find(st => st.isrc === tt.isrc);
-    if (!spotifyTrack) return;
-    cacheMap.push([spotifyTrack.id, tt])
+    if (spotifyTrack) cacheTrack(spotifyTrack.id, tt);
   });
-
-  cache(cacheMap);
 }
 
 export const searchCache = ({tracks, onFail}: ImportTracksParams) => {
   const cachedTracks: ITrack[] = [];
   const tracksToImport: ITrack[] = [];
   tracks.forEach(t => {
-    const data = localStorage.getItem(`${CACHE_KEY_PREFIX}:${t.id}`);
+    const data = localStorage.getItem(getKey(t.id));
 
     if (data === CACHE_MATCH_FAIL_MARKER) onFail([t]);
     else if (data) cachedTracks.push(JSON.parse(data));
